9429ff45b1fc1034b0f98a650a5dbb05e87ae9e2
tomoakiWeb <tomomiura0026@gmail.com>
2026-02-25 23:37:38 +0900

    Create CounterAppShortcuts

---
+++ CounterAppIntent/CounterAppShortcuts.swift
@@ -0,0 +1,37 @@
+import AppIntents
+
+struct CounterAppShortcuts: AppShortcutsProvider {
+    static var appShortcuts: [AppShortcut] {
+        AppShortcut(
+            intent: IncrementCounterIntent(),
+            phrases: [
+                "Increment \(.applicationName) counter",
+                "Increase \(.applicationName) counter",
+                "Add one to \(.applicationName)"
+            ],
+            shortTitle: "Increment",
+            systemImageName: "plus.circle"
+        )
+        
+        AppShortcut(
+            intent: DecrementCounterIntent(),
+            phrases: [
+                "Decrement \(.applicationName) counter",
+                "Decrease \(.applicationName) counter",
+                "Subtract one from \(.applicationName)"
+            ],
+            shortTitle: "Decrement",
+            systemImageName: "minus.circle"
+        )
+        
+        AppShortcut(
+            intent: ResetCounterIntent(),
+            phrases: [
+                "Reset \(.applicationName) counter",
+                "Clear \(.applicationName) counter"
+            ],
+            shortTitle: "Reset",
+            systemImageName: "arrow.counterclockwise"
+        )
+    }
+}
