d53c50035fc1899204d3b9806884a45678a3791c
tomoakiWeb <tomomiura0026@gmail.com>
2026-02-25 23:10:05 +0900

    Initial Commit

---
+++ CounterAppIntent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CounterAppIntent
+//
+//  Created by 三浦知明 on 2026/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CounterAppIntent/CounterAppIntentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppIntentApp.swift
+//  CounterAppIntent
+//
+//  Created by 三浦知明 on 2026/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppIntentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
